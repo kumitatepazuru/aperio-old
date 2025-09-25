cbe86ac44470cdf3655044e6080ea88ebf749032
kumitatepazuru <43287589+kumitatepazuru@users.noreply.github.com>
2025-09-25 22:11:40 +0900

    💚 windows環境で環境変数が入っていない問題の応急処置とninjaのインストールをする

--- scripts/build/opencv/opencv.sh
+++ scripts/build/opencv/opencv.sh
@@ -6,7 +6,7 @@ mv opencv-python-headless-*/ opencv
 cd opencv
 
 # https://docs.opencv.org/4.x/db/d05/tutorial_config_reference.html
-export CMAKE_ARGS="-D CMAKE_BUILD_TYPE=RELEASE \
+CMAKE_ARGS="-D CMAKE_BUILD_TYPE=RELEASE \
     -D CPU_DISPATCH=AVX,AVX2 \
     -D WITH_OPENCL=ON \
     -D WITH_V4L=OFF \
@@ -15,9 +15,7 @@ export CMAKE_ARGS="-D CMAKE_BUILD_TYPE=RELEASE \
     -D HIGHGUI_ENABLE_PLUGINS=OFF \
     -D WITH_GTK=OFF \
     -D WITH_WIN32API=OFF \
-    -D BUILD_opencv_python3=ON"
-
-MAKEFLAGS="-j$(nproc)" pip wheel . --verbose
+    -D BUILD_opencv_python3=ON" MAKEFLAGS="-j$(nproc)" pip wheel . --verbose
 
 mkdir -p ../src-tauri/binaries/wheels/
 cp -v opencv_python_headless*.whl ../src-tauri/binaries/wheels/
